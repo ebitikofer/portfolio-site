315c838c6f2821fcfcd1803690a145e4d6670461
Eric Bitikofer <e.bitikofer@gmail.com>
2020-07-19 21:58:43 -0400

    nav link component

---
+++ resources/js/components/NavigationLinks.vue
@@ -0,0 +1,102 @@
+<template>
+
+    <div class="links">
+        <a :key="index" v-for="(llink, index) in llinks" :id="llink.id" :href="llink.url" :class="{ 'route-link': true, 'current': llink.isCurrent }">{{ llink.id }}</a>
+        <!-- <a id="home" href="/">Home</a>
+        <a id="blog" href="blog">Blog</a>
+        <a id="portfolio" href="portfolio">Portfolio</a>
+        <a id="github" href="https://github.com/ebitikofer">GitHub</a>
+        <a id="art" href="art">Art</a>
+        <a id="about" href="about">About</a> -->
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        // name: 'navigation-links',
+        props: {
+            current: String,
+            // links: Array
+        },
+        data: () => {
+            return {
+                llinks: [
+                    {
+                        id: 'home',
+                        url: '/',
+                        isCurrent: false
+                    },
+                    // {
+                    //     id: 'blog',
+                    //     url: 'blog',
+                    //     isCurrent: false
+                    // },
+                    {
+                        id: 'portfolio',
+                        url: 'portfolio',
+                        isCurrent: false
+                    },
+                    {
+                        id: 'github',
+                        url: 'https://github.com/ebitikofer',
+                        isCurrent: false
+                    },
+                    // {
+                    //     id: 'art',
+                    //     url: 'art',
+                    //     isCurrent: false
+                    // },
+                    // {
+                    //     id: 'about',
+                    //     url: 'about',
+                    //     isCurrent: false
+                    // },
+                ]
+            }
+        },
+        mounted() {
+            console.log('Hello');
+            this.llinks.forEach(link => {
+                link.isCurrent = false;
+                if (this.current === link.id) {
+                    link.isCurrent = true;
+                }
+            });
+            this.dayExpr = this.links
+        },
+        watch: {
+            current: function () {
+                console.log(this.current);
+                this.llinks.forEach(link => {
+                    link.isCurrent = false;
+                    if (this.current === link.id) {
+                        link.isCurrent = true;
+                    }
+                });
+                this.dayExpr = this.links
+            }
+        },
+    }
+    
+</script>
+
+<style>
+
+.route-link {
+    color: #fff;
+    padding: 0 25px;
+    font-size: 13px;
+    font-weight: 600;
+    letter-spacing: 0.1rem;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+.current {
+    color: #999;
+    cursor: default;
+}
+
+</style>
