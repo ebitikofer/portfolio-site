ae974aec944dc5482c8eb9c13767a525d271de8d
Eric Bitikofer <e.bitikofer@gmail.com>
2020-07-31 02:24:36 -0400

    fix upload link

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -5,7 +5,7 @@
     <div id="app">
 
         <?php
-            $links = '[{ "id": "< portfolio", "url": "/portfolio", "isCurrent": false }, { "id": "upload", "url": "/project/create/{{ $project->id }}", "isCurrent": false }]';
+            $links = '[{ "id": "< portfolio", "url": "/portfolio", "isCurrent": false }, { "id": "upload", "url": "/project/create/'.$project->id.'", "isCurrent": false }]';
         ?>
 
         <div class="flex-center position-ref quarter-height">
@@ -18,30 +18,30 @@
 
         @if(count($project->photos) > 0)
 
-        <?php
-            $colcount = count($project->photos);
-            $i = 1;
-        ?>
+            <?php
+                $colcount = count($project->photos);
+                $i = 1;
+            ?>
 
-        <div id="photos">
-            <div class="grid-x text-center">
-            @foreach($project->photos as $photo)
-                @if($i == $colcount)
-                <div class="cell small-4 end">
-                    <portfolio-image :link="`/project/{{ $photo->id }}`" :src="`/storage/photos/{{ $photo->project_id }}/{{ $photo->photo }}`" :title="`{{ $photo->title }}`" :delay="{{ $i }}"></portfolio-image>
-                @else
-                <div class="cell small-4">
-                    <portfolio-image :link="`/project/{{ $photo->id }}`" :src="`/storage/photos/{{ $photo->project_id }}/{{ $photo->photo }}`" :title="`{{ $photo->title }}`" :delay="{{ $i }}"></portfolio-image>
-                @endif
-                @if($i % 3 == 0)
-                    </div></div><div class="grid-x text-center">
-                @else
-                    </div>
-                @endif
-                <?php $i++; ?>
-            @endforeach
+            <div id="photos">
+                <div class="grid-x text-center projects-outer">
+                @foreach($project->photos as $photo)
+                    @if($i == $colcount)
+                    <div class="cell small-4 end">
+                        <portfolio-image :link="`/project/{{ $photo->id }}`" :src="`/storage/photos/{{ $photo->project_id }}/{{ $photo->photo }}`" :title="`{{ $photo->title }}`" :delay="{{ $i }}"></portfolio-image>
+                    @else
+                    <div class="cell small-4">
+                        <portfolio-image :link="`/project/{{ $photo->id }}`" :src="`/storage/photos/{{ $photo->project_id }}/{{ $photo->photo }}`" :title="`{{ $photo->title }}`" :delay="{{ $i }}"></portfolio-image>
+                    @endif
+                    @if($i % 3 == 0)
+                        </div></div><div class="grid-x text-center">
+                    @else
+                        </div>
+                    @endif
+                    <?php $i++; ?>
+                @endforeach
+                </div>
             </div>
-        </div>
 
         @else
 
